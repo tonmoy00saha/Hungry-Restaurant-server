379d5d99342ffce03893a5d960f872509698d8b8
tonmoy00saha <tonmoy.official.20@gmail.com>
2024-12-23 02:59:34 +0600

    delete data

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.szqv4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -40,10 +40,24 @@ async function run() {
     })
 
     // Carts Collection
+    app.get('/carts',async(req,res)=>{
+      const email = req.query.email;
+      const query = {email:email};
+
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
 
     app.post('/carts', async(req,res)=>{
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    })
+
+    app.delete('/carts/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
 
